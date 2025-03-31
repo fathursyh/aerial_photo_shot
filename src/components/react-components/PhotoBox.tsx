ca23633e15968d88ac219e4f2bd818f3d37120ff
fathursyh <fathur.syah1@gmail.com>
2025-03-31 23:41:13 +0700

    update : teleport modal

--- src/components/react-components/PhotoBox.tsx
+++ src/components/react-components/PhotoBox.tsx
@@ -1,5 +1,6 @@
 import { useRef, useState } from "react";
 import BasicModal from "./composables/BasicModal";
+import { createPortal } from "react-dom";
 
 export default function PhotoBox() {
     let index = 0;
@@ -13,8 +14,8 @@ export default function PhotoBox() {
     async function startCamera() {
         setModal(true);
         if (isStreaming) return;
-        await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'user' }, audio: false }).then(mediaStream => {
-            stream.current = mediaStream
+        await navigator.mediaDevices.getUserMedia({ video: { facingMode: "user" }, audio: false }).then((mediaStream) => {
+            stream.current = mediaStream;
             video.current!.srcObject = mediaStream;
             video.current!.play();
             setIsStreaming(true);
@@ -27,19 +28,19 @@ export default function PhotoBox() {
                 setFlash(false);
                 clearTimeout(timeout);
             }, 150);
-            const photo = document.querySelectorAll('.photo');
-            canvas.current!.width = 250,
-                canvas.current!.height = 170,
-                canvas.current!.getContext('2d')?.drawImage((video.current as CanvasImageSource), 0, 0, canvas.current?.width!, canvas.current!.height);
-            const imageUrl = canvas.current?.toDataURL('image/jpeg');
+            const photo = document.querySelectorAll(".photo");
+            (canvas.current!.width = 250),
+                (canvas.current!.height = 170),
+                canvas.current!.getContext("2d")?.drawImage(video.current as CanvasImageSource, 0, 0, canvas.current?.width!, canvas.current!.height);
+            const imageUrl = canvas.current?.toDataURL("image/jpeg");
             (photo[index] as HTMLImageElement).src = imageUrl!;
             setCount(3);
             index++;
-        } 
+        }
     }
     function setTimer() {
         const secondInterval = setInterval(() => {
-            setCount(count => count - 1);
+            setCount((count) => count - 1);
         }, 1000);
         const interval = setInterval(() => {
             takePhoto();
@@ -51,34 +52,34 @@ export default function PhotoBox() {
         }, 3000);
     }
     function stopVideo() {
+        if (!isStreaming) return;
         setModal(false);
         video.current!.pause();
-        stream.current!.getTracks().forEach(track => track.stop());
+        stream.current!.getTracks().forEach((track) => track.stop());
         video.current!.srcObject = null;
         setIsStreaming(false);
     }
     return (
-            <>
-            <BasicModal max={true} show={modal} closeModal={stopVideo} confirm={setTimer}>
-                <span className="absolute bg-black/70 z-20 grid place-items-center w-10 h-10 rounded text-white text-2xl font-bold">{count}</span>
-                <section>
-                    <div className="relative rounded-lg overflow-hidden mb-2">
-                        <div className={`w-full h-full bg-white absolute transition-all duration-100 opacity-0 ${flash && 'opacity-100'}`} />
-                        <video ref={video} className={`w-[1000px] bg-neutral-800`} />
-                    </div>
-                    <div className="flex justify-center gap-4">
-
-                    </div>
-                </section>
-            </BasicModal>
+        <>
+            {createPortal(
+                <BasicModal max={true} show={modal} closeModal={stopVideo} confirm={setTimer}>
+                    <span className="absolute bg-black/70 z-20 grid place-items-center w-10 h-10 rounded text-white text-2xl font-bold">{count}</span>
+                    <section>
+                        <div className="relative rounded-lg overflow-hidden mb-2">
+                            <div className={`w-full h-full bg-white absolute transition-all duration-100 opacity-0 ${flash && "opacity-100"}`} />
+                            <video ref={video} className={`w-[1000px] bg-neutral-800`} />
+                        </div>
+                        <div className="flex justify-center gap-4"></div>
+                    </section>
+                </BasicModal>,
+                document.body
+            )}
             <aside className="bg-white p-8 flex flex-col gap-4 shadow-md rounded cursor-pointer hover:shadow-glow2 hover:shadow-accent transition-shadow duration-200" onClick={startCamera}>
-                <canvas ref={canvas} style={{ display: 'none' }}></canvas>
-                {
-                    ([...Array(4).keys()]).map((item) => (
-                        <img width={250} height={170} className="rounded object-cover object-center photo max-h-[200px] overflow-hidden bg-neutral-600" key={item} />
-                    ))
-                }
+                <canvas ref={canvas} style={{ display: "none" }}></canvas>
+                {[...Array(4).keys()].map((item) => (
+                    <img width={250} height={170} className="rounded object-cover object-center photo max-h-[200px] overflow-hidden bg-neutral-600" key={item} />
+                ))}
             </aside>
-            </>
+        </>
     );
 }
